cac2cc6c3c21842a50b7dace04f0b38145916afe
Pralexio <159443612+Pralexio@users.noreply.github.com>
2024-09-19 19:06:12 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,6 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Exposer des fonctions dans le contexte de la page front-end
 contextBridge.exposeInMainWorld('electronAPI', {
     selectFolder: () => ipcRenderer.invoke('select-folder'),
     onLoadFolder: (callback) => ipcRenderer.on('load-folder', (event, folderPath) => callback(folderPath))
